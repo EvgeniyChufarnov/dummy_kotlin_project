fa0b0f8844cf16d40ae8d792b43b21261f4e475b
Evgeniy Chufarnov <evgeniychufarnov@gmail.com>
2021-07-01 19:16:46 +0300

    switched to kotlin

--- app/src/main/java/com/example/dummy_kotlin_project/MainActivity.kt
+++ app/src/main/java/com/example/dummy_kotlin_project/MainActivity.kt
@@ -1,19 +1,17 @@
-package com.example.dummy_kotlin_project;
+package com.example.dummy_kotlin_project
 
-import android.os.Bundle;
-import android.widget.Toast;
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-import androidx.appcompat.app.AppCompatActivity;
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        findViewById(R.id.btn_show_toast).setOnClickListener(v ->
-                Toast.makeText(this, "Some text", Toast.LENGTH_LONG).show()
-        );
+        findViewById<View>(R.id.btn_show_toast).setOnClickListener {
+            Toast.makeText(this, getString(R.string.some_text), Toast.LENGTH_LONG).show()
+        }
     }
 }
